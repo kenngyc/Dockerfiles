3e724ecd6d05bb4ce3d82f3d634399af8293cdfa
Ken Ng <ken.ng@mulesoft.com>
2016-09-30 12:14:31 +0800

    Fixed linux random name generator
    Removed user input on length parameter.

--- mule-ee-3.8/startDockerContainer.sh
+++ mule-ee-3.8/startDockerContainer.sh
@@ -23,7 +23,7 @@ if [ "$(uname)" == "Darwin" ]; then
     containerName=mule-$version-$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # GNU/Linux platform detected
-    containerName=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-8} | head -n 1)
+    containerName=mule-$version-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 fi
 
 echo "Starting new mule container: "$containerName
